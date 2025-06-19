d43c0006e32e88db88bae289420d20fcd52f7858
galdoben <galdobenedetta@gmail.com>
2025-06-19 18:09:15 +0200

    add ex three

--- src/main.ts
+++ src/main.ts
@@ -26,11 +26,45 @@ type Dipendente = {
   anniDiServizio: number[],
 }
 
-const dipendente1: Dipendente = {
-  nome: "Jasdine",
-  cognome: "Galdo",
-  annoNascita: 1993,
-  sesso: "f",
-  anniDiServizio: [2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023]
+// const dipendente1: Dipendente = {
+//   nome: "Jasdine",
+//   cognome: "Galdo",
+//   annoNascita: 1993,
+//   sesso: "f",
+//   anniDiServizio: [2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023]
+// }
+// console.log(dipendente1);
+
+type Developer = Dipendente & {
+  livelloEsperienza: "Junior" | "Mid" | "Senior",
+  linguaggi?: string[],
+  certificazioni: string[],
 }
-console.log(dipendente1);
+
+type ProjectManager = Dipendente & {
+  teamSize: number | null,
+  budgetGestito?: number,
+  stakeholderPrincipali: string[]
+}
+
+const developer1: Developer = {
+  nome: "Alicia",
+  cognome: "Caldo",
+  annoNascita: 1998,
+  sesso: "f",
+  anniDiServizio: [2018, 2019, 2020],
+  livelloEsperienza: "Mid",
+  certificazioni: ["React", "TypeScript"]
+}
+console.log(developer1);
+
+const projectManager1: ProjectManager = {
+  nome: "Marco",
+  cognome: "Rossi",
+  annoNascita: 1985,
+  sesso: "m",
+  anniDiServizio: [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021],
+  teamSize: null,
+  stakeholderPrincipali: ["Cliente A", "Cliente B"]
+}
+console.log(projectManager1);
